c5e73311ccf514abc29f5c2ec6a2978dcffe2268
xhesiand <xhesiand@gmail.com>
2014-12-22 11:32:45 +0100

    multiple mongos server address

--- data-discovery/src/it/csi/smartdata/odata/dbconfig/MongoSingleton.java
+++ data-discovery/src/it/csi/smartdata/odata/dbconfig/MongoSingleton.java
@@ -1,7 +1,9 @@
 package it.csi.smartdata.odata.dbconfig;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import com.mongodb.MongoClient;
@@ -19,16 +21,29 @@ public class MongoSingleton {
 
 		credential = MongoCredential.createMongoCRCredential(mongoParams.get("MONGO_USERNAME"), mongoParams.get("MONGO_DB_AUTH"), mongoParams.get("MONGO_PASSWORD").toCharArray());
 
+		List<ServerAddress> servers =new ArrayList<ServerAddress>();
 
-		ServerAddress serverAdd;
-
-		serverAdd = new ServerAddress(mongoParams.get("MONGO_HOST"), Integer.parseInt(mongoParams.get("MONGO_PORT")));
-
+//		serverAdd = new ServerAddress(mongoParams.get("MONGO_HOST"), Integer.parseInt(mongoParams.get("MONGO_PORT")));
+		String[] hosts = mongoParams.get("MONGO_HOST").split(";");
+		String[] ports = mongoParams.get("MONGO_PORT").split(";");
+		ServerAddress serverAdd=null;
+		for(int i=0;i<hosts.length;i++){
+			String addr = hosts[i];
+			Integer port = null;
+			try{
+				port= Integer.parseInt(ports[i]);
+			}catch(Exception e){
+				port=27017;
+			}
+			serverAdd = new ServerAddress(addr,port);
+			if(addr!=null && !"".equals(addr))
+				servers.add(serverAdd);
+		}
 
 		if("true".equals(mongoParams.get("MONGO_DB_AUTH_FLAG")))
-			mongo = new MongoClient(serverAdd,Arrays.asList(credential));
+			mongo = new MongoClient(servers,Arrays.asList(credential));
 		else
-			mongo = new MongoClient(serverAdd);
+			mongo = new MongoClient(servers);
 
 
 	}
