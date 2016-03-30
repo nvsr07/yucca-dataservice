7ae52e3244e4763629138400b86c57a6f6c3e79b
afranceschetti <alessandro.franceschetti@csi.it>
2016-03-30 17:02:27 +0200

    Metadata API - Resource Service

---
+++ metadata-api/src/main/java/org/csi/yucca/dataservice/metadataapi/delegate/resources/Streams.java
@@ -0,0 +1,16 @@
+package org.csi.yucca.dataservice.metadataapi.delegate.resources;
+
+public class Streams {
+	private Stream stream;
+
+	public Streams() {
+	}
+
+	public Stream getStream() {
+		return stream;
+	}
+
+	public void setStream(Stream stream) {
+		this.stream = stream;
+	}
+}
---
+++ metadata-api/src/main/java/org/csi/yucca/dataservice/metadataapi/delegate/resources/StreamsForIcon.java
@@ -0,0 +1,18 @@
+package org.csi.yucca.dataservice.metadataapi.delegate.resources;
+
+public class StreamsForIcon {
+	private Streams streams;
+
+	public StreamsForIcon() {
+
+	}
+
+	public Streams getStreams() {
+		return streams;
+	}
+
+	public void setStreams(Streams streams) {
+		this.streams = streams;
+	}
+
+}
