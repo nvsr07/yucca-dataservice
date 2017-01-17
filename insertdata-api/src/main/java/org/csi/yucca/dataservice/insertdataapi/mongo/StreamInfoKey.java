3ffd8d9ccc1d2e0c669aa9f4bfdff61fc7d600a5
Claudio Parodi <gigaclood@gmail.com>
2017-01-17 14:33:46 +0100

    stream equals

--- insertdata-api/src/main/java/org/csi/yucca/dataservice/insertdataapi/mongo/StreamInfoKey.java
+++ insertdata-api/src/main/java/org/csi/yucca/dataservice/insertdataapi/mongo/StreamInfoKey.java
@@ -19,6 +19,47 @@ public class StreamInfoKey implements Serializable{
 	
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sensor == null) ? 0 : sensor.hashCode());
+		result = prime * result + ((stream == null) ? 0 : stream.hashCode());
+		result = prime * result + ((tenant == null) ? 0 : tenant.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StreamInfoKey other = (StreamInfoKey) obj;
+		if (sensor == null) {
+			if (other.sensor != null)
+				return false;
+		} else if (!sensor.equals(other.sensor))
+			return false;
+		if (stream == null) {
+			if (other.stream != null)
+				return false;
+		} else if (!stream.equals(other.stream))
+			return false;
+		if (tenant == null) {
+			if (other.tenant != null)
+				return false;
+		} else if (!tenant.equals(other.tenant))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
 	public String toString() {
 		return "StreamInfoKey [tenant=" + tenant + ", stream=" + stream
 				+ ", sensor=" + sensor + "]";
