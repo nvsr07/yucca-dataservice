ad929c6071c19d2d5682c8063cb3573bb1d21de4
Claudio Parodi <gigaclood@gmail.com>
2017-01-31 10:53:48 +0100

    prova mapping

--- insertdata-api/src/main/java/org/csi/yucca/dataservice/insertdataapi/model/output/ErrorOutput.java
+++ insertdata-api/src/main/java/org/csi/yucca/dataservice/insertdataapi/model/output/ErrorOutput.java
@@ -1,23 +1,31 @@
 package org.csi.yucca.dataservice.insertdataapi.model.output;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+	
+
 public class ErrorOutput {
 
 	private String errorName=null;
 	private String errorCode=null;
 	private String output=null;
 	private String errorMessage=null;
+	
+	
+	@JsonProperty("error_name")
 	public String getErrorName() {
 		return errorName;
 	}
 	public void setErrorName(String errorName) {
 		this.errorName = errorName;
 	}
+	@JsonProperty("error_code")
 	public String getErrorCode() {
 		return errorCode;
 	}
 	public void setErrorCode(String errorCode) {
 		this.errorCode = errorCode;
 	}
+	
 	public String getOutput() {
 		return output;
 	}
