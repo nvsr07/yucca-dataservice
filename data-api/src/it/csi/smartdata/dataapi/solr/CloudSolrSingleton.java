318531078c64849832c461fef44c1801d802966b
Claudio Parodi <gigaclood@gmail.com>
2017-05-23 14:12:50 +0200

    prova basic

--- data-api/src/it/csi/smartdata/dataapi/solr/CloudSolrSingleton.java
+++ data-api/src/it/csi/smartdata/dataapi/solr/CloudSolrSingleton.java
@@ -2,6 +2,11 @@ package it.csi.smartdata.dataapi.solr;
 
 import it.csi.smartdata.dataapi.constants.SDPDataApiConfig;
 
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.HttpClientUtil;
@@ -23,9 +28,21 @@ public class CloudSolrSingleton {
 			System.out.println("------------------>>>> DOPOOOOOOOOOOOOOOOO java.security.auth.login.config --- " + System.getProperty("java.security.auth.login.config"));
 			System.out.println("------------------>>>> DOPOOOOOOOOOOOOOOOO solr.kerberos.jaas.appname ---" + System.getProperty("solr.kerberos.jaas.appname"));
 			System.out.println("------------------>>>> DOPOOOOOOOOOOOOOOOO javax.security.auth.useSubjectCredsOnly ---" + System.getProperty("javax.security.auth.useSubjectCredsOnly"));
-						
+				// prova
+			HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+			if (SDPDataApiConfig.instance.getSolrUsername()!=null)
+			{
+				CredentialsProvider provider = new BasicCredentialsProvider();
+				UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
+						SDPDataApiConfig.instance.getSolrUsername(), SDPDataApiConfig.instance.getSolrPassword());
+				provider.setCredentials(AuthScope.ANY, credentials);
+				clientBuilder.setDefaultCredentialsProvider(provider);
+			}
+			clientBuilder.setMaxConnTotal(128);
+			//prova
 			
-			server = new CloudSolrClient(SDPDataApiConfig.getInstance().getSolrUrl());
+			
+			server = new CloudSolrClient(SDPDataApiConfig.getInstance().getSolrUrl(),clientBuilder.build());
 		} catch (Exception e) {
 			
 		}
