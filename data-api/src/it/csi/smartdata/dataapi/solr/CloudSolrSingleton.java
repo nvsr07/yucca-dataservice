9f541c133f50a0a32615506679d354f0079f4f09
fcorsan <fabrizio.corsanego@csi.it>
2017-05-23 14:51:12 +0200

    odata phoenix ... ripristino fron kerberos

--- data-api/src/it/csi/smartdata/dataapi/solr/CloudSolrSingleton.java
+++ data-api/src/it/csi/smartdata/dataapi/solr/CloudSolrSingleton.java
@@ -2,15 +2,8 @@ package it.csi.smartdata.dataapi.solr;
 
 import it.csi.smartdata.dataapi.constants.SDPDataApiConfig;
 
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
-import org.apache.solr.client.solrj.impl.HttpClientUtil;
-import org.apache.solr.client.solrj.impl.Krb5HttpClientConfigurer;
 
 public class CloudSolrSingleton {
 	//private CloudSolrClient server;
@@ -19,30 +12,8 @@ public class CloudSolrSingleton {
 	private CloudSolrSingleton() {
 		try {
 			
-			System.out.println("------------------>>>> PRIMAAAAAAAAAAAAAAAAAA " + System.getProperty("java.security.auth.login.config"));
 			
-			System.setProperty("java.security.auth.login.config", "/appserv/jboss/ajb620/part001node01/standalone/configuration/jaas-client.conf");			
-			System.setProperty("solr.kerberos.jaas.appname", "Client");			
-			HttpClientUtil.setConfigurer( new Krb5HttpClientConfigurer());
-
-			System.out.println("------------------>>>> DOPOOOOOOOOOOOOOOOO java.security.auth.login.config --- " + System.getProperty("java.security.auth.login.config"));
-			System.out.println("------------------>>>> DOPOOOOOOOOOOOOOOOO solr.kerberos.jaas.appname ---" + System.getProperty("solr.kerberos.jaas.appname"));
-			System.out.println("------------------>>>> DOPOOOOOOOOOOOOOOOO javax.security.auth.useSubjectCredsOnly ---" + System.getProperty("javax.security.auth.useSubjectCredsOnly"));
-				// prova
-			HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-			if (SDPDataApiConfig.instance.getSolrUsername()!=null)
-			{
-				CredentialsProvider provider = new BasicCredentialsProvider();
-				UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
-						SDPDataApiConfig.instance.getSolrUsername(), SDPDataApiConfig.instance.getSolrPassword());
-				provider.setCredentials(AuthScope.ANY, credentials);
-				clientBuilder.setDefaultCredentialsProvider(provider);
-			}
-			clientBuilder.setMaxConnTotal(128);
-			//prova
-			
-			
-			server = new CloudSolrClient(SDPDataApiConfig.getInstance().getSolrUrl(),clientBuilder.build());
+			server = new CloudSolrClient(SDPDataApiConfig.getInstance().getSolrUrl());
 		} catch (Exception e) {
 			
 		}
