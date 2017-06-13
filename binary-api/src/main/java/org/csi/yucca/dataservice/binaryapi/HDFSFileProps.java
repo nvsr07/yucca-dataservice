9f4547a0760316a11e7fa70bb5c1960fb785779c
fcorsan <fabrizio.corsanego@csi.it>
2017-06-13 11:14:54 +0200

    binaryai - csv all version

---
+++ binary-api/src/main/java/org/csi/yucca/dataservice/binaryapi/HDFSFileProps.java
@@ -0,0 +1,30 @@
+package org.csi.yucca.dataservice.binaryapi;
+
+public class HDFSFileProps {
+
+	
+	private String fullFilePath=null;
+	private int maxFileds=0;
+	private int datasetVersion=0;
+	public String getFullFilePath() {
+		return fullFilePath;
+	}
+	public void setFullFilePath(String fullFilePath) {
+		this.fullFilePath = fullFilePath;
+	}
+	public int getMaxFileds() {
+		return maxFileds;
+	}
+	public void setMaxFileds(int maxFileds) {
+		this.maxFileds = maxFileds;
+	}
+	public int getDatasetVersion() {
+		return datasetVersion;
+	}
+	public void setDatasetVersion(int datasetVersion) {
+		this.datasetVersion = datasetVersion;
+	}
+	
+	
+	
+}
