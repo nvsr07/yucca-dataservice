470c290c66ca3a58f6954636c6b8c0446f68bd92
gigaclood <gigaclood@gmail.com>
2017-07-01 20:15:10 +0200

    Update MongoTenantDbSingleton.java

--- data-api/src/it/csi/smartdata/dataapi/mongo/MongoTenantDbSingleton.java
+++ data-api/src/it/csi/smartdata/dataapi/mongo/MongoTenantDbSingleton.java
@@ -137,7 +137,7 @@ public class MongoTenantDbSingleton {
 			MongoClient mongoClient = null;
 			if (SDPDataApiConfig.getInstance().getMongoDefaultPassword()!=null && SDPDataApiConfig.getInstance().getMongoDefaultPassword().trim().length()>0 && 
 					SDPDataApiConfig.getInstance().getMongoDefaultUser()!=null && SDPDataApiConfig.getInstance().getMongoDefaultUser().trim().length()>0	) {
-				MongoCredential credential = MongoCredential.createMongoCRCredential(SDPDataApiConfig.getInstance().getMongoDefaultUser(), 
+				MongoCredential credential = MongoCredential.createCredential(SDPDataApiConfig.getInstance().getMongoDefaultUser(), 
 						"admin", 
 						SDPDataApiConfig.getInstance().getMongoDefaultPassword().toCharArray());
 				mongoClient = new MongoClient(arrServerAddr,Arrays.asList(credential),options);
