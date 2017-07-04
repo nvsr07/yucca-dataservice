cd66c5c2c80a6d7bb55d501202abc1be6946aefa
fcorsan <fabrizio.corsanego@csi.it>
2017-07-04 16:10:51 +0200

    odata phoenix - " in fields name phoenix

--- data-api/src/it/csi/smartdata/dataapi/odata/SDPPhoenixExpressionVisitor.java
+++ data-api/src/it/csi/smartdata/dataapi/odata/SDPPhoenixExpressionVisitor.java
@@ -453,7 +453,8 @@ public class SDPPhoenixExpressionVisitor implements ExpressionVisitor {
 			
 			
 			
-			String nome = (fieldAppendMap.get(this.entitySetName+"."+cur.getExpression().getUriLiteral()) == null ?cur.getExpression().getUriLiteral() : fieldAppendMap.get(this.entitySetName+"."+cur.getExpression().getUriLiteral())); 
+			String nome = (fieldAppendMap.get(this.entitySetName+"."+cur.getExpression().getUriLiteral()) == null ?cur.getExpression().getUriLiteral() : fieldAppendMap.get(this.entitySetName+"."+cur.getExpression().getUriLiteral()));
+			nome="\""+nome+"\"";
 			if (ret==null) ret = nome+ " " + cur.getSortOrder();
 			else ret+=", "+nome+ " " + cur.getSortOrder();
 			
@@ -504,10 +505,13 @@ public class SDPPhoenixExpressionVisitor implements ExpressionVisitor {
 	
 	
 	
-	
-	
-	
 	private String getFullFielName(String fieldNameInput,EdmType type) {
+		return "\""+getFullFielName( fieldNameInput, type,false)+"\"";
+	}
+	
+	
+	
+	private String getFullFielName(String fieldNameInput,EdmType type,boolean dummyboolean ) {
 		String ret=this.fieldAppendMap.get(this.entitySetName+"."+fieldNameInput);
 		if (ret==null) ret = fieldNameInput;
 		
