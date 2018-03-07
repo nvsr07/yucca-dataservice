a486e5dc236387e33cb3a09bf645c97159ceb0fc
gianfranco.stolfa <gianfranco.stolfa@stolfa.exage.com>
2018-03-07 15:57:04 +0100

    utility

---
+++ admin-api/src/main/java/org/csi/yucca/adminapi/response/builder/OrganizationResponseBuilder.java
@@ -0,0 +1,31 @@
+package org.csi.yucca.adminapi.response.builder;
+
+import org.csi.yucca.adminapi.model.Organization;
+import org.csi.yucca.adminapi.response.OrganizationResponse;
+
+public class OrganizationResponseBuilder{
+
+	// model
+	private Organization model;
+
+	public OrganizationResponseBuilder(Organization organization) {
+		super();
+		this.model = organization;
+	}
+
+	public OrganizationResponse build(){
+		
+		if (this.model == null) {
+			return null;
+		}
+		
+		OrganizationResponse response = new OrganizationResponse();
+		
+		response.setIdOrganization(this.model.getIdOrganization());
+		response.setDescription(this.model.getDescription());
+		response.setOrganizationcode(this.model.getOrganizationcode());
+		
+		return response;
+	}
+
+}
